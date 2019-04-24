6005cfaafec4009b7c1ba43d7c60f03336847e5a
RaphaelSBarros <49734953+RaphaelSBarros@users.noreply.github.com>
2019-04-24 20:42:21 -0300

    Lista

---
+++ ListaQ1.py
@@ -0,0 +1,21 @@
+import math
+A = (2 * 4 == 24 / 3)
+print(A)
+
+B = (15 % 4) < (19 % 6)
+print(B)
+
+C = (2 < 5) != (8 == 8)
+print(C)
+
+D = True or False
+print(D)
+
+E = 2 ** 2 == math.pow(2, 2)
+print(E)
+
+F = False and ((7 / 2) > 3.5)
+print(F)
+
+G = ( math.ceil(2.5) == 3) and (math.sqrt(9) == 3)
+print(G)
